5bf74002b7f8e3dd52dcc5df32e8b76bc9f0928f
27239 <mokariyay5@gmail.com>
2026-05-18 17:41:11 +0530

    i

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Deploy index.html to local Apache server
-# Change the path if needed
-DESTINATION="/var/www/html"
-echo "Deploying index.html to $DESTINATION..."
-sudo cp index.html $DESTINATION
-echo "Deployment done!"
+DEST="/var/www/html"
+echo "Deploying index.html..."
+sudo cp index.html $DEST
+echo "Deployment complete!"
